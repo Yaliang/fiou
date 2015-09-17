b2f60212405d898f4b564418c5f8daee251ff3dd
Yaliang Wang <yaliangwang2015@u.northwestern.edu>
2015-09-16 22:52:29 -0500

    add qr code for a user

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 Parse.initialize("ZnLjW6xuqieYfVfFJZB5nuRONBybVGCjJTS0T8El", "rci8OKWaBeYZSaC6kAwLr30RhmmFFPUWrDCe1ZqG");
 
+
+
 user = {
-	autologin: function() {
+	id: "",
+	username: "",
+	autologin: function(options) {
 		var currentUser = Parse.User.current()
 
+		options = options || {}
+
 		if (currentUser) {
-			pt.loadPage("home")
+			user.id = currentUser.id
+			user.username = currentUser.getUsername()
+			pt.loadPage(options.dest || "home")
 		} else {
 			pt.loadPage("login")
 		}
@@ -14,34 +22,49 @@ user = {
 		var myname = $("#username").val()
 		var mypass = $("#password").val()
 		Parse.User.logIn(myname, mypass, {
-			success: function(user) {
+			success: function(userObj) {
 				$("#loginerror").html("")
+				console.log(userObj)
+				user.id = userObj.id
+				user.username = userObj.getUsername()
 				pt.loadPage("home")
 			},
 			error: function(error) {
-				$("#loginerror").html(JSON.parse(JSON.stringify(error)).message)
+				var loginerrormsg = "<div style='display:none' class='alert alert-warning alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>"+JSON.parse(JSON.stringify(error)).message+"</div>"
+				$("#loginerror").html(loginerrormsg)
+				$("#loginerror div:first-child").slideDown()
 			}
 		});
 	},
 	signup: function() {
-		var myname = $("#set-username").val()
-		var mypass = $("#set-password").val()
+		var myname = $("#signup-username").val()
+		var mypass = $("#signup-password").val()
 
-		var user = new Parse.User()
-		user.set("username", myname)
-		user.set("password", mypass)
-		user.set("email", myname)
+		var myuser = new Parse.User()
+		myuser.set("username", myname)
+		myuser.set("password", mypass)
 
-		user.signUp(null, {
-			success: function(user) {
+		myuser.signUp(null, {
+			success: function(userObj) {
 				$("#signuperror").html("")
+				console.log(userObj)
+				user.id = userObj.id
+				user.username = userObj.getUsername()
 				pt.loadPage("home")
 			},
-			error: function(user, error) {
+			error: function(userObj, error) {
 				console.log(error)
-				$("#signuperror").html(JSON.parse(JSON.stringify(error)).message)
+				var signuperrormsg = "<div style='display:none' class='alert alert-warning alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>"+JSON.parse(JSON.stringify(error)).message+"</div>"
+				$("#signuperror").html(signuperrormsg)
+				$("#signuperror div:first-child").slideDown()
+
 			}
 		})
+	},
+	logout: function() {
+		Parse.User.logOut()
+
+		pt.loadPage("login")
 	}
 }
 
